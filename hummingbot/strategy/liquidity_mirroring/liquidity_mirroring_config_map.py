de419ed4222a1eda124583feb802aaa96614fd24
Keith O'Neill <keith@tokamak.ai>
2020-04-01 11:47:10 -0400

    made a more sensible config file and start method to go with it

--- hummingbot/strategy/liquidity_mirroring/liquidity_mirroring_config_map.py
+++ hummingbot/strategy/liquidity_mirroring/liquidity_mirroring_config_map.py
@@ -8,30 +8,16 @@ from hummingbot.client.settings import (
     EXAMPLE_PAIRS,
 )
 
-
-def is_valid_primary_market_trading_pair(value: str) -> bool:
+def is_valid_mirroring_market_trading_pair(value: str) -> bool:
     primary_market = liquidity_mirroring_config_map.get("primary_market").value
-    return is_valid_market_trading_pair(primary_market, value)
+    mirrored_market = liquidity_mirroring_config_map.get("mirrored_market").value
+    return (is_valid_market_trading_pair(primary_market, value) and is_valid_market_trading_pair(mirrored_market, value))
 
-
-def is_valid_secondary_market_trading_pair(value: str) -> bool:
-    secondary_market = liquidity_mirroring_config_map.get("mirrored_market").value
-    return is_valid_market_trading_pair(secondary_market, value)
-
-
-def primary_trading_pair_prompt():
+def mirror_trading_pair_prompt():
     primary_market = liquidity_mirroring_config_map.get("primary_market").value
     example = EXAMPLE_PAIRS.get(primary_market)
-    return "Enter the token trading pair you would like to trade on %s%s >>> " \
+    return "Enter the token trading pair you would like to mirror on %s%s >>> " \
            % (primary_market, f" (e.g. {example})" if example else "")
-
-
-def secondary_trading_pair_prompt():
-    secondary_market = liquidity_mirroring_config_map.get("mirrored_market").value
-    example = EXAMPLE_PAIRS.get(secondary_market)
-    return "Enter the token trading pair you would like to trade on %s%s >>> " \
-           % (secondary_market, f" (e.g. {example})" if example else "")
-
 
 liquidity_mirroring_config_map = {
     "primary_market": ConfigVar(
@@ -44,13 +30,9 @@ liquidity_mirroring_config_map = {
         prompt="Enter the name of the exchange which you would like to mirror >>> ",
         validator=is_exchange,
         on_validated=lambda value: required_exchanges.append(value)),
-    "primary_market_trading_pair": ConfigVar(
-        key="primary_market_trading_pair",
-        prompt=primary_trading_pair_prompt,
-        validator=is_valid_primary_market_trading_pair),
-    "secondary_market_trading_pair": ConfigVar(
-        key="secondary_market_trading_pair",
-        prompt=secondary_trading_pair_prompt,
-        validator=is_valid_secondary_market_trading_pair),
+    "market_trading_pair_to_mirror": ConfigVar(
+        key="market_trading_pair_to_mirror",
+        prompt=mirror_trading_pair_prompt,
+        validator=is_valid_mirroring_market_trading_pair),
 }
 
