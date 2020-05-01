eb0b3f1b4422c90871e2dc07eb3f7a976104dfc4
Keith O'Neill <keith@tokamak.ai>
2020-05-01 10:08:05 -0400

    integrated liquidity_mirroring strat into hummingbot update

--- hummingbot/strategy/liquidity_mirroring/liquidity_mirroring_config_map.py
+++ hummingbot/strategy/liquidity_mirroring/liquidity_mirroring_config_map.py
@@ -1,7 +1,7 @@
 from hummingbot.client.config.config_var import ConfigVar
 from hummingbot.client.config.config_validators import (
-    is_exchange,
-    is_valid_market_trading_pair,
+    validate_exchange,
+    validate_market_trading_pair,
 )
 from hummingbot.client.settings import (
     required_exchanges,
@@ -10,12 +10,12 @@ from hummingbot.client.settings import (
 
 def is_valid_mirroring_market_trading_pair(value: str) -> bool:
     mirrored_market = liquidity_mirroring_config_map.get("mirrored_market").value
-    in_mirrored_market = is_valid_market_trading_pair(mirrored_market, value)
+    in_mirrored_market = validate_market_trading_pair(mirrored_market, value)
     return in_mirrored_market
 
 def is_valid_primary_market_trading_pair(value: str) -> bool:
     primary_market = liquidity_mirroring_config_map.get("primary_market").value
-    in_primary_market = is_valid_market_trading_pair(primary_market, value)
+    in_primary_market = validate_market_trading_pair(primary_market, value)
     return in_primary_market
 
 def mirror_trading_pair_prompt():
@@ -31,24 +31,32 @@ def primary_trading_pair_prompt():
            % (primary_market, f" (e.g. {example})" if example else "")
 
 liquidity_mirroring_config_map = {
+    "strategy": ConfigVar(key="strategy",
+                          prompt="",
+                          default="liquidity_mirroring"
+                          ),
     "primary_market": ConfigVar(
         key="primary_market",
         prompt="Enter your primary exchange name >>> ",
-        validator=is_exchange,
+        validator=validate_exchange,
+        prompt_on_new=True,
         on_validated=lambda value: required_exchanges.append(value)),
     "mirrored_market": ConfigVar(
         key="mirrored_market",
         prompt="Enter the name of the exchange which you would like to mirror >>> ",
-        validator=is_exchange,
+        validator=validate_exchange,
+        prompt_on_new=True,
         on_validated=lambda value: required_exchanges.append(value)),
     "market_trading_pair_to_mirror": ConfigVar(
         key="market_trading_pair_to_mirror",
         prompt=mirror_trading_pair_prompt,
+        prompt_on_new=True,
         validator=lambda value: is_valid_mirroring_market_trading_pair(value),
     ),
     "primary_market_trading_pair": ConfigVar(
         key="primary_market_trading_pair",
         prompt=primary_trading_pair_prompt,
+        prompt_on_new=True,
         validator=lambda value: is_valid_primary_market_trading_pair(value),
     ),
     "equivalent_tokens": ConfigVar(
@@ -62,27 +70,32 @@ liquidity_mirroring_config_map = {
         key="two_sided_mirroring",
         prompt="Two-Sided Mirroring Threshold in base currency (inf for one-sided) >>> ",
         default=float("inf"),
+        prompt_on_new=True,
         type_str="float" 
     ),
     "spread_percent": ConfigVar(
         key="spread_percent",
         prompt="Enter your desired spread percentage >>> ",
         default=0.001,
+        prompt_on_new=True,
         type_str="float"
     ),
     "max_exposure_base": ConfigVar(
         key="max_exposure_base",
         prompt="Enter the max desired exposure for the base asset >>> ",
+        prompt_on_new=True,
         type_str="float"
     ),
     "max_exposure_quote": ConfigVar(
         key="max_exposure_quote",
         prompt="Enter the max desired exposure for the quote asset >>> ",
+        prompt_on_new=True,
         type_str="float"
     ),
     "max_offset_loss": ConfigVar(
         key="max_offset_loss",
         prompt="Enter your maximum tolerated offset loss >>> ",
+        prompt_on_new=True,
         type_str="float"
     )
 }
