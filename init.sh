3606d5e44b33382e1b6eb1f461f612cecc55029c
John Doe <you@example.com>
2020-07-28 09:39:26 -0400

    Added persistent volume yamls; updated values.yaml to take config file names; updated init.sh to auto import on deploy

--- init.sh
+++ init.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 tmux new-session -d -s hummingbot -n hbstrat
-tmux send-keys -t hummingbot:hbstrat " /opt/conda/envs/hummingbot/bin/python3 /bin/hummingbot_quickstart.py" Enter
+tmux send-keys -t hummingbot:hbstrat "python3 bin/hummingbot_quickstart.py" Enter
+
+sleep 5
+
+tmux send-keys -t hummingbot:hbstrat "import ${STRAT_CONF_FILE}" Enter
+
 #GET PID by parsing?? or Force PID
 #TAIL PID
 tail -f /dev/null
 
-# # To attach run 
-#tmux attach -t hummingbot:hbstrat
+###### To attach to Hummingbot Session run
+##   tmux attach -t hummingbot:hbstrat
 
 
